9fe9915f52886c9d5de5b337ab7ab15e4f40f784
Kennata <kenata966@gmail.com>
2026-03-28 16:39:10 +0700

    Added Header

--- my-FoodLy-app/src/App.jsx
+++ my-FoodLy-app/src/App.jsx
@@ -1,8 +1,13 @@
+import Header from "./components/Header"
 import TestPage from "./pages/testPage"
 
 function App() {
   return(
-    <TestPage></TestPage>
+    <>
+      <Header></Header>
+      <TestPage></TestPage>
+    </>
+    
   )
 }
 
